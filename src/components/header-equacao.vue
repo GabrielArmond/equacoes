a0094730a70947f2723864ba16388e1a0a30941d
Gabriel Armond <gabriel.guerra@farme.com.br>
2022-06-17 17:41:51 -0300

    fix: ajustes semanticos

--- src/components/header-equacao.vue
+++ src/components/header-equacao.vue
@@ -4,7 +4,7 @@
       <p class="pa-3 text-h3">Calculadora inteligente de Engenharia</p>
     </v-row>
     <p class="text-subtitle-1">
-      Trabalho realizado com intuito de facilitar calculos específicos para
+      Trabalho realizado com intuito de facilitar cálculos específicos para
       fixação e sustentação de parafusos no teto.
     </p>
   </v-col>
